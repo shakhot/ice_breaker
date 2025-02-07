8eef050537e296a19e1787bbe857778ab3629592
Sha Khot <shakhotai@gmail.com>
2025-02-07 15:00:48 -0800

    My app changes.

---
+++ ice_breaker.py
@@ -0,0 +1,57 @@
+
+from typing import Tuple
+
+from dotenv import load_dotenv
+
+#from langchain import PromptTemplate
+#from langchain.chat_models import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_community.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+
+#from langchain_openai import ChatOpenAI
+
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
+from output_parsers import person_intel_parser, PersonIntel
+from third_parties.linkedin import scrape_linkedin_profile
+from third_parties.twitter import scrape_user_tweets
+
+
+def ice_break(name: str) -> Tuple[PersonIntel, str]:
+    linkedin_profile_url = linkedin_lookup_agent(name=name )
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url,mock=True)
+
+    twitter_username = twitter_lookup_agent(name=name)
+    tweets = scrape_user_tweets(username=twitter_username, num_tweets=2,mock=True)
+
+    summary_template = """
+         given the Linkedin information {linkedin_information} and twitter {twitter_information} about a person from I want you to create:
+         1. a short summary
+         2. two interesting facts about them
+         3. A topic that may interest them
+         4. 2 creative Ice breakers to open a conversation with them
+        \n{format_instructions}
+     """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["linkedin_information", "twitter_information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
+    )
+
+    llm = ChatOpenAI(temperature=1, model_name="gpt-4o-mini")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    result = chain.run(linkedin_information=linkedin_data, twitter_information=tweets)
+    return person_intel_parser.parse(result), linkedin_data.get("profile_pic_url")
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Ice Breaker Enter")
+    ice_break(name="Shashank Khot")
---
+++ ice_breaker_linkedintweet.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+from langchain.prompts.prompt import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
+from third_parties.twitter import scrape_user_tweets
+
+
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+
+    twitter_username = twitter_lookup_agent(name=name)
+    tweets = scrape_user_tweets(username=twitter_username)
+
+    summary_template = """
+    given the information about a person from linkedin {information},
+    and their latest twitter posts {twitter_posts} I want you to create:
+    1. A short summary
+    2. two interesting facts about them 
+
+    Use both information from twitter and Linkedin
+    """
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information", "twitter_posts"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
+
+    chain = summary_prompt_template | llm
+
+    res = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
+
+    print(res)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Ice Breaker Enter")
+    ice_break_with(name="Harrison Chase")
